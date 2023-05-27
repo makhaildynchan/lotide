12fe466e774bf48c9dbc22a66347e54ed0e0fa7c
Khaildyn Chan <makhaildynchan@gmail.com>
2023-05-26 18:03:32 -0600

    count appearance of letters in a given word

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else (console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`));
+};
+
+const countLetters = function(words) {
+  const results = {};
+  for (const letter of words) {
+    //each letter in the allItems array
+    if (letter !== " ") {
+      if (results[letter] === undefined) {
+        results[letter] = 1;
+      } else {
+        results[letter] += 1;
+      }
+    }
+  }
+  console.log(results);
+  return results;
+};
+
+countLetters('kedss');

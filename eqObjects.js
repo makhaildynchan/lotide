0944305da1ff14fcc49e8da8352493af01d9520f
Khaildyn Chan <makhaildynchan@gmail.com>
2023-05-27 19:58:40 -0600

    returns true if two objects are equal

---
+++ eqObjects.js
@@ -0,0 +1,35 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return console.log('false');
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+
+  for (const item in object1) {
+    if (Array.isArray(object1[item])) {
+      if (!eqArrays(object1[item], object2[item])) {
+        return false;
+      }
+    } else if (object1[item] !== object2[item]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+console.log(eqObjects(shirtObject, anotherShirtObject));
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+console.log(eqObjects(shirtObject, longSleeveShirtObject));

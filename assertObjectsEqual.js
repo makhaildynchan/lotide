d57dab2b4609621c1c367f4606f1db967a5f7b0e
Khaildyn Chan <makhaildynchan@gmail.com>
2023-05-27 20:03:42 -0600

    asserts eqObjects.js

---
+++ assertObjectsEqual.js
@@ -0,0 +1,44 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return console.log('false');
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+
+  for (const item in object1) {
+    if (Array.isArray(object1[item])) {
+      if (!eqArrays(object1[item], object2[item])) {
+        return false;
+      }
+    } else if (object1[item] !== object2[item]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  // import library - util, library's inspect function
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertObjectsEqual(shirtObject, anotherShirtObject);
+assertObjectsEqual(shirtObject, longSleeveShirtObject);

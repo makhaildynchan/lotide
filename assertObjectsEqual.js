9bff9bc11f8a49124b17372c82ed08a0d8bbf02f
Khaildyn Chan <makhaildynchan@gmail.com>
2023-06-13 20:44:43 -0600

    takes 2 objects; returns boolean

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,44 +1,21 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return console.log('false');
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqObjects = require("./eqObjects");
 
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  }
+const assertObjectsEqual = function(object1, object2) {
+  const inspect = require("util").inspect;
 
-  for (const item in object1) {
-    if (Array.isArray(object1[item])) {
-      if (!eqArrays(object1[item], object2[item])) {
-        return false;
-      }
-    } else if (object1[item] !== object2[item]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertObjectsEqual = function(actual, expected) {
-  // import library - util, library's inspect function
-  const inspect = require('util').inspect;
-  if (eqObjects(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  if (eqObjects(object1, object2)) {
+    console.log(
+      `✅ Assertion Passed ${inspect(object1)} is the same to [${inspect(
+        object2
+      )}]`
+    );
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    console.log(
+      `❌ Assertion Failed ${inspect(object1)} is different to ${inspect(
+        object2
+      )}`
+    );
   }
 };
 
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject = { size: "medium", color: "red" };
-const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
-assertObjectsEqual(shirtObject, anotherShirtObject);
-assertObjectsEqual(shirtObject, longSleeveShirtObject);
+module.exports = assertObjectsEqual;

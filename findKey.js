9c14792b8bb1b29bc04c9e12ff6e3ae11b0a447d
Khaildyn Chan <makhaildynchan@gmail.com>
2023-06-13 21:07:27 -0600

    returns first key that meets callback criteria

--- findKey.js
+++ findKey.js
@@ -1,3 +1,4 @@
+//takes in an object and callback and returns the first key that meets the criteria specified in callback.
 const findKey = function(object, callback) {
   for (const key in object) {
     if (callback(object[key])) {

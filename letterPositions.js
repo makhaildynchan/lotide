3b03bd79a7c64fa01400bb1eec651458c0206665
Khaildyn Chan <makhaildynchan@gmail.com>
2023-06-13 21:02:37 -0600

    takes string;returns all letter position indices

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,5 @@
+const assertArraysEqual = require("./assertArraysEqual");
+
 const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
@@ -15,3 +17,6 @@ const letterPositions = function(sentence) {
 
 console.log(letterPositions("lighthouse in the house"));
 console.log(letterPositions("hello"));
+assertArraysEqual(letterPositions("hello").e, [1]);
+
+module.exports = letterPositions;

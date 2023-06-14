e90c77e09036d116c43e1ed8651fb50d774e3d4d
Khaildyn Chan <makhaildynchan@gmail.com>
2023-06-13 21:04:48 -0600

    compares 2 objects

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,4 @@
+//a function that takes in 2 objects and returns true if they are equal and false otherwise.
 const eqObjects = require("./eqObjects");
 
 const assertObjectsEqual = function(object1, object2) {

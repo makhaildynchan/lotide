2a29205958410efdd2765481063392320e1be499
Khaildyn Chan <makhaildynchan@gmail.com>
2023-06-13 20:56:38 -0600

    returns the first element of input array

--- head.js
+++ head.js
@@ -1,15 +1,9 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else (console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`));
-// };
-const assertEqual = require('./assertEqual');
 const head = function(array) {
-  return array[0];
+  if (array.length >= 1) {
+    return array[0];
+  } else {
+    return undefined;
+  }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 module.exports = head;

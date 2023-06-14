da2432fea81edb6eb3303c9794b8757b381ad1bc
Khaildyn Chan <makhaildynchan@gmail.com>
2023-06-13 20:33:00 -0600

    testcase for eqArrays.js

---
+++ test/eqArraysTest.js
@@ -0,0 +1,14 @@
+/**
+ * input 2 arrays and boolean
+ * input function eqArrays
+ * input function assertEqual
+ * return boolean
+ */
+const assertEqual = require("../assertEqual");
+const eqArrays = require("../eqArrays");
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false);
+assertEqual(eqArrays([1, 2, "3"], [1, 2, "3"]), true);
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, "3"]), false);
+assertEqual(eqArrays([], []), true);

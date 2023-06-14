ff69a9ab98118692de67614584e27f2b88749f63
Khaildyn Chan <makhaildynchan@gmail.com>
2023-06-13 20:55:44 -0600

    nested arrays will be flatten

--- flatten.js
+++ flatten.js
@@ -1,27 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return console.log('false');
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false; s
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    //compares two arrays passed as arguments
-    console.log(`✅ Assertion Passed: [${array1}] is the same to [${array2}]`);
-    return true;
-  } else {
-    console.log(`❌ Assertion Failed: [${array1}] is different to [${array2}]`);
-    return false;
-  }
-};
-
 const flatten = function(elements) {
   // console.log(elements);
   let flatArray = [];
@@ -36,3 +12,5 @@ const flatten = function(elements) {
   return flatArray;
 };
 flatten([1, 2, [3, 4], 5, [6]]);
+
+module.exports = flatten;

4f3fc3d1f8308c85e3269a7dee1510b82f23691e
Khaildyn Chan <makhaildynchan@gmail.com>
2023-06-13 21:27:04 -0600

    returns array except first element

--- tail.js
+++ tail.js
@@ -1,16 +1,8 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else (console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`));
-// };
-
-const assertEqual = require('./assertEqual');
 const tail = function(arr) {
   return arr.slice(1);
 };
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 console.log(tail(words));
-assertEqual(words.length, 3);
 
 module.exports = tail;

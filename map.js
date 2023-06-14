b942af98c1fd16541674bc4dbcd1883797335f75
Khaildyn Chan <makhaildynchan@gmail.com>
2023-06-13 21:13:41 -0600

    returns new array based on the result of callback

--- map.js
+++ map.js
@@ -1,3 +1,6 @@
+//takes in an array and a callback and returns a new array based on the results of the callback on each item in the array.
+const assertArraysEqual = require("./assertArraysEqual");
+
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -6,5 +9,11 @@ const map = function(array, callback) {
   return results;
 };
 
-let words = ["ground", "control", "to", "major", "tom"];
+const words = ["ground", "control", "to", "major", "tom"];
+const num = [1, 2, 3];
+
 console.log(map(words, word => word[0]));
+let example2 = map(num, (num) => num * 10);
+assertArraysEqual(example2, [10, 20, 30, 40, 50]);
+
+module.exports = map;
